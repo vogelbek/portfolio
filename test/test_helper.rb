2e02c3f146597f03542d9547f72287c10a218f7e
Adam Hendricksen <adam@nird.us>
2014-01-11 19:47:02 -0800

    Configuring minitest with capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
-
-# Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/rails/capybara"
+require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
