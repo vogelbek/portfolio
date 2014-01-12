fe4a31ab8a5080335a99b0a8ed0747f3b486e325
Adam Hendricksen <adam@nird.us>
2014-01-11 19:55:13 -0800

    Testing for Rails 404 page

---
+++ test/features/has404_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class Has404Test < Capybara::Rails::TestCase
+  test "page doesn't exist" do
+    visit '/404'
+    assert_content page, "The page you were looking for doesn't exist."
+    refute_content page, "Welcome aboard"
+  end
+end
