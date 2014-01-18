107cd1993ce472cece35f13a8a0c79ffdeae0f22
Adam Hendricksen <adam@nird.us>
2014-01-17 20:25:13 -0800

    Renaming test for root to make it accurate

--- test/features/has_home_test.rb
+++ test/features/has_home_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class HasHomeTest < Capybara::Rails::TestCase
-  test "sanity" do
+  test "Root has my name" do
     visit root_path
     assert_content page, "Adam Hendricksen"
   end
