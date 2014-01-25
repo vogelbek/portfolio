e8a81632a1711f3c49e18109e50463c976a0ebef
Adam Hendricksen <adam@nird.us>
2014-01-25 00:41:11 -0800

    Specifying the index path.
    Starts #1

---
+++ test/features/posts_index_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "PostsIndex" do
+  scenario "a potential employer visits the blog index" do
+    visit posts_path
+    page.must_have_content posts(:one).title
+    page.must_have_content posts(:two).body
+  end
+end
