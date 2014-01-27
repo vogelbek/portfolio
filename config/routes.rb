b9d352326797b43e2861a67dbc5cc8fdc876af59
Adam Hendricksen <adam@nird.us>
2014-01-26 22:18:00 -0800

    Naming the route, linking to it from the blog post partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Portfolio::Application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#edit', as: 'post_edit'
   put 'posts/:id', to: 'posts#update', as: 'post'
 
   # The priority is based upon order of creation: first created -> highest priority.
