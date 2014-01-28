36057f6271840d2ab5a8cdba53fdc9a69bd47183
Adam Hendricksen <adam@nird.us>
2014-01-27 21:36:15 -0800

    Adding feature test

---
+++ test/features/delete_post_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+feature "DeletePost" do
+  scenario "a post can be deleted" do
+    visit posts_path
+    page.must_have_content posts(:one).title
+
+    click_on "Delete #{posts(:one).title}"
+
+    page.must_not_have_content posts(:one).body
+  end
+end
