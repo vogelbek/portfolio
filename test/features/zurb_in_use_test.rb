93d087ad39f55a0035b5d609caacc666012dba68
Adam Hendricksen <adam@nird.us>
2014-01-28 16:46:50 -0800

    Adding test to ensure Zurb Foundation is being compiled

---
+++ test/features/zurb_in_use_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+feature "ZurbInUse" do
+  scenario "determine if compiled Zurb is in css" do
+    visit '/assets/application.css?body=1'
+
+    page.must_have_content 'zurb'
+  end
+end
