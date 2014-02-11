72078113de4a6dbf34bc1c0010e65840170bc278
Adam Hendricksen <adam@nird.us>
2014-02-11 15:10:32 -0800

    Adding basic validation to projects

--- test/features/projects/create_project_test.rb
+++ test/features/projects/create_project_test.rb
@@ -10,4 +10,12 @@ feature 'CreateProject' do
 
     page.must_have_content projects(:one).description
   end
+
+  scenario 'Cant create without Description' do
+    visit '/projects/new'
+
+    fill_in 'Description', with: ''
+
+    current_path.must_equal '/projects/new'
+  end
 end
