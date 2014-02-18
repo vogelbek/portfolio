5224a63ce47dbe8523a6973ef22af1e7d087f07a
Adam Hendricksen <adam@nird.us>
2014-02-17 23:51:10 -0800

    add an admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create email: ENV['ADMIN_EMAIL'],
+             password: ENV['ADMIN_PSSWD'],
+             password_confirmation: ENV['ADMIN_PSSWD']
