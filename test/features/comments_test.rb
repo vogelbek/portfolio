ce6fbb3d803c06f53b80de74145fd28e739acd57
Adam Hendricksen <adam@nird.us>
2014-04-21 16:07:16 -0700

    Smashing bug for empty comments

--- test/features/comments_test.rb
+++ test/features/comments_test.rb
@@ -1,9 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
-feature "Project comments" do
-  scenario "Projects list comments" do
+feature 'Project comments' do
+  scenario 'Projects list comments' do
     visit project_path 1
     page.must_have_content comments(:one).comment
     page.must_have_content comments(:two).tldr
+  end
+  scenario 'Projects without comments should not have empty braces' do
+    visit project_path 2
+    page.wont_have_content comments(:one).comment
+    page.wont_have_content '[]'
   end
 end
